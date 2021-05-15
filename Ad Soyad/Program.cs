117e1cfa9afab7e4ca2691abe1130a152b84cb1b
Oğuzhan Akbulut <oguzhanakbulut@trakya.edu.tr>
2021-05-15 14:35:00 +0300

    Ad Soyad

---
+++ Ad Soyad/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ad_Soyad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\n");
+
+            Console.Write("Adınızı Girin: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Soyadınızı Girin: ");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("\nMerhaba, " + name + " " + surname);
+        }
+    }
+}
